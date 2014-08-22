7d46736ff4f0eb983e6a7ca5b82a8ab882d9d0c7
vitrio wibisono <akito@localhost.localdomain>
2014-08-22 21:13:34 +0700

    tidy up directory for login

--- edit.php
+++ edit.php
@@ -6,19 +6,19 @@ if (version_compare(PHP_VERSION, '5.3.7', '<')) {
 } else if (version_compare(PHP_VERSION, '5.5.0', '<')) {
     // if you are using PHP 5.3 or PHP 5.4 you have to include the password_api_compatibility_library.php
     // (this library adds the PHP 5.5 password hashing functions to older versions of PHP)
-    require_once('libraries/password_compatibility_library.php');
+    require_once('system/login/libraries/password_compatibility_library.php');
 }
 // include the config
-require_once('config/config.php');
+require_once('system/login/config/config.php');
 
 // include the to-be-used language, english by default. feel free to translate your project and include something else
-require_once('translations/en.php');
+require_once('system/login/translations/en.php');
 
 // include the PHPMailer library
-require_once('libraries/PHPMailer.php');
+require_once('system/login/libraries/PHPMailer.php');
 
 // load the login class
-require_once('classes/Login.php');
+require_once('system/login/classes/Login.php');
 
 // create a login object. when this object is created, it will do all login/logout stuff automatically
 // so this single line handles the entire login process.
@@ -28,10 +28,10 @@ $login = new Login();
 if ($login->isUserLoggedIn() == true) {
     // the user is logged in. you can do whatever you want here.
     // for demonstration purposes, we simply show the "you are logged in" view.
-    include("views/edit.php");
+    include("system/login/views/edit.php");
 
 } else {
     // the user is not logged in. you can do whatever you want here.
     // for demonstration purposes, we simply show the "you are not logged in" view.
-    include("views/not_logged_in.php");
+    include("system/login/views/not_logged_in.php");
 }
--- index.php
+++ index.php
@@ -19,16 +19,16 @@ if (version_compare(PHP_VERSION, '5.3.7', '<')) {
     require_once('libraries/password_compatibility_library.php');
 }
 // include the config
-require_once('config/config.php');
+require_once('system/login/config/config.php');
 
 // include the to-be-used language, english by default. feel free to translate your project and include something else
-require_once('translations/en.php');
+require_once('system/login/translations/en.php');
 
 // include the PHPMailer library
-require_once('libraries/PHPMailer.php');
+require_once('system/login/libraries/PHPMailer.php');
 
 // load the login class
-require_once('classes/Login.php');
+require_once('system/login/classes/Login.php');
 
 // create a login object. when this object is created, it will do all login/logout stuff automatically
 // so this single line handles the entire login process.
@@ -38,11 +38,11 @@ $login = new Login();
 if ($login->isUserLoggedIn() == true) {
     // the user is logged in. you can do whatever you want here.
     // for demonstration purposes, we simply show the "you are logged in" view.
-     include("views/logged_in.php");
+     include("system/login/views/logged_in.php");
     //  include ("views/main.php");
 } else {
     // the user is not logged in. you can do whatever you want here.
     // for demonstration purposes, we simply show the "you are not logged in" view.
-     include("views/not_logged_in.php");
+     include("system/login/views/not_logged_in.php");
     //  include ("views/main.php");
 }
