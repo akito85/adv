be1299e5eb923c0faffb51f91710e456813efdf3
vitrio wibisono <akito@localhost.localdomain>
2014-08-23 21:21:40 +0700

    fix url and reload after submit, and add comments for signin and signup

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+  // signup
   $("#sign-up-form").submit(function(event) {
     $.post("register.php", $("#sign-up-form").serialize() + "&register=Register").done(function(data) {
       alert(data);
@@ -7,11 +8,13 @@ $(document).ready(function(){
     event.preventDefault();
   });
 
+  // signin
   $("#sign-in-form").submit(function(event) {
     $.post("index.php", $("#sign-in-form").serialize() + "&login=Login").done(function(data) {
       //alert(data);
+      window.location.replace("index.php");
+      location.reload();
     });
-    window.location.replace("index.php");
     //$('#sign-in-form')[0].reset();
     event.preventDefault();
   });
