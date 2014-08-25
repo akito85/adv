e289f1bdedc5cb5d0e33ef773eaf2ffc4957e906
vitrio wibisono <akito@localhost.localdomain>
2014-08-25 08:39:00 +0700

    add pattern for name and phone while signing up, and add custom error message

--- system/layout/overlay-page.php
+++ system/layout/overlay-page.php
@@ -15,9 +15,11 @@
 	<form id="sign-up-form">
 		<div class="logo-copy"></div>
 		<p>Get started.</p>
-		<input placeholder="Name" type="text" name="sign-up-name" id="sign-up-name" required />
-		<input placeholder="Phone" type="text" name="sign-up-phone" id="sign-up-phone" required />
-		<input placeholder="Email Address" type="email" name="sign-up-email" id="sign-up-email" required />
+		<input placeholder="Name" type="text" name="sign-up-name" id="sign-up-name" 
+		       oninvalid="this.setCustomValidity('Username cannot be empty, only letters and space allowed.')" 
+           onchange="this.setCustomValidity('')" pattern="[a-zA-Z ]{6,64}" required />
+		<input placeholder="Phone" type="text" name="sign-up-phone" id="sign-up-phone" pattern="[0-9]{6,64}" required />
+		<input placeholder="Email Address" type="email" name="sign-up-email" id="sign-up-email" autocomplete="off" required />
 		<input placeholder="Password" type="password" name="sign-up-password" id="sign-up-password" autocomplete="off" required />
 		<input placeholder="Password Confirmation" type="password" name="sign-up-password-confirmation" id="sign-up-password-confirmation" autocomplete="off" required />
     <input type="submit" name="sign-up-submit" id="sign-up-submit" value="Signup">
