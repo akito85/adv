6da2ab9a8eb3dbdd649536b5a3c9aa5671e64f16
vitrio wibisono <akito@localhost.localdomain>
2014-08-26 20:48:24 +0700

    adding error msg style, adding error msg div to register/signup form, add custom validation msg, and adding password pattern

--- system/layout/overlay-page.php
+++ system/layout/overlay-page.php
@@ -1,29 +1,31 @@
 <div class="overlay-page">
-	<div class="overlay-background"></div>
-	<form id="sign-in-form">
-		<a href="/"><div class="logo-copy"></div></a>
-		<p>Sign in</p>
-		<input placeholder="Email Address" type="email" name="sign-in-email" id="sign-in-email" autocomplete="off" required />
-		<input placeholder="Password" type="password" name="sign-in-password" id="sign-in-password" autocomplete="off" required />
-		<input type="submit" name="sign-in-submit" id="sign-in-submit" value="Login">
-		<p>Don't have an account yet?</p>
-		<a class="sign-up-btn" href="javascript:void(0)">Sign up</a>
-	</form>
+  <div class="overlay-background"></div>
+  <form id="sign-in-form">
+    <a href="/"><div class="logo-copy"></div></a>
+    <p>Sign in</p>
+    <input placeholder="Email Address" type="email" name="sign-in-email" id="sign-in-email" autocomplete="off" required />
+    <input placeholder="Password" type="password" name="sign-in-password" id="sign-in-password" autocomplete="off" required />
+    <input type="submit" name="sign-in-submit" id="sign-in-submit" value="Login">
+    <p>Don't have an account yet?</p>
+    <a class="sign-up-btn" href="javascript:void(0)">Sign up</a>
+  </form>
 
 <!-- show registration form, but only if we didn't submit already -->
-<?php if (!$registration->registration_successful && !$registration->verification_successful) { ?>	
-	<form id="sign-up-form">
-		<div class="logo-copy"></div>
-		<div class="error-message" id="error-message"></div>
-		<p>Get started.</p>
-		<input placeholder="Name" type="text" name="sign-up-name" id="sign-up-name" 
-		       oninvalid="this.setCustomValidity('Username cannot be empty, only letters and space allowed.')" 
+<?php if (!$registration->registration_successful && !$registration->verification_successful) { ?>  
+  <form id="sign-up-form">
+    <div class="logo-copy"></div>
+    <div class="error-message" id="error-message"></div>
+    <p>Get started.</p>
+    <input placeholder="Name" type="text" name="sign-up-name" id="sign-up-name" 
+           oninvalid="this.setCustomValidity('Username cannot be empty, only letters and space allowed.')" 
            onchange="this.setCustomValidity('')" pattern="[a-zA-Z\s]{6,64}" required />
-		<input placeholder="Phone" type="text" name="sign-up-phone" id="sign-up-phone" pattern="[0-9]{6,20}" required />
-		<input placeholder="Email Address" type="email" name="sign-up-email" id="sign-up-email" autocomplete="off" required />
-		<input placeholder="Password" type="password" name="sign-up-password" id="sign-up-password" autocomplete="off" required />
-		<input placeholder="Password Confirmation" type="password" name="sign-up-password-confirmation" id="sign-up-password-confirmation" autocomplete="off" required />
+    <input placeholder="Phone" type="text" name="sign-up-phone" id="sign-up-phone" pattern="[0-9]{6,20}" required />
+    <input placeholder="Email Address" type="email" name="sign-up-email" id="sign-up-email" autocomplete="off" required />
+    <input placeholder="Password" type="password" name="sign-up-password" id="sign-up-password" 
+           onchange="form.sing-up-password-confirmation.pattern = this.value;"
+           pattern="[\w+\d\s]{6,20}" autocomplete="off" required />
+    <input placeholder="Password Confirmation" type="password" name="sign-up-password-confirmation" id="sign-up-password-confirmation" pattern="[\w+\d\s]{6,20}" autocomplete="off" required />
     <input type="submit" name="sign-up-submit" id="sign-up-submit" value="Signup">
-	</form>
+  </form>
 <?php } ?>
 </div>
