b638816e2545ed6ec95b5cd348827f23eae4526c
vitrio wibisono <akito@localhost.localdomain>
2014-09-20 15:24:42 +0700

    removing old media query with standard bootstrap media query, removing navbar-brand old style, fix navbar layout, adding navbar right, adding bootstrap js to script

--- system/layout/nav.php
+++ system/layout/nav.php
@@ -2,22 +2,23 @@
   <!-- We use the fluid option here to avoid overriding the fixed width of a normal container within the narrow content columns. -->
   <div class="container-fluid">
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-8">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapsed">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#"></a>
+      <a class="navbar-brand" href="#"><img src="../assets/img/logo-copy.png"/></a>
+
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse">
+    <div class="collapse navbar-collapse" id="collapsed">
       <ul class="nav navbar-nav">
-        <li <?php if ($_REQUEST['id'] =="home"){echo "class='active'";} ?>><a href="../home/">Home</a></li>
-        <li <?php if ($_REQUEST['id'] =="about"){echo "class='active'";} ?>><a href="../about/">About Us</a></li>
-        <li <?php if ($_REQUEST['id'] =="autoshow"){echo "class='active'";} ?>><a href="../autoshow/">Autoshow</a></li>
-        <li <?php if ($_REQUEST['id'] =="products"){echo "class='active'";} ?>><a href="../products/">Products</a></li>
+        <li <?php if ($_REQUEST['id'] =="home"){echo "class='active'";} ?>><a class="right-line" href="../home/">Home</a></li>
+        <li <?php if ($_REQUEST['id'] =="about"){echo "class='active'";} ?>><a class="right-line" href="../about/">About Us</a></li>
+        <li <?php if ($_REQUEST['id'] =="autoshow"){echo "class='active'";} ?>><a class="right-line" href="../autoshow/">Autoshow</a></li>
+        <li <?php if ($_REQUEST['id'] =="products"){echo "class='active'";} ?>><a class="right-line" href="../products/">Products</a></li>
         <li>
           <?php if ($login->isUserLoggedIn() == true) { ?>
             <a style="margin-left: 16px" class="adv-red-btn" href="../logout/" id="logout-btn">Logout</a>
@@ -26,15 +27,15 @@
           <?php }?>
         </li>
       </ul>
-      <?php if ($login->isUserLoggedIn() == true) { ?>
-        <div class="cart">
-          <div class="greetings">Welcome, <strong><?php echo $login->getUsername(); ?></strong></div>
-          <a href="index.php?id=my-cart"><span class="badge">0</span><span class="icon-cart"></span>My Cart</a>
-        </div>
-      <?php
-            }
-      ?>     
 
+      <ul class="nav navbar-nav navbar-right">
+        <?php if ($login->isUserLoggedIn() == true) { ?>
+          <li><a class="greetings right-line" href="#">Welcome, <strong><?php echo $login->getUsername(); ?></strong></a></li>
+          <li><a href="#"><span class="badge">0</span><span class="icon-cart"></span>My Cart</a></li>
+        <?php
+              }
+        ?>
+      </ul>    
     </div><!-- /.navbar-collapse -->
   </div>
 </nav>
--- system/layout/script.php
+++ system/layout/script.php
@@ -1,4 +1,5 @@
 <script src="../assets/js/jquery-2.1.1.min.js"></script>
+<script src="../assets/bootstrap-3.2.0-dist/js/bootstrap.min.js"></script>
 <script src="../assets/js/pace.min.js"></script>
 <script src="../assets/js/main.js"></script>
 <script src="../assets/js/ajax.js"></script>
