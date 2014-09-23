84cdff0368d860ac88fbc1c7fc7b500da11a5e0b
vitrio wibisono <akito@localhost.localdomain>
2014-09-23 23:10:58 +0700

    added profile dropdown

--- system/layout/nav.php
+++ system/layout/nav.php
@@ -30,7 +30,18 @@
 
       <ul class="nav navbar-nav navbar-right">
         <?php if ($login->isUserLoggedIn() == true) { ?>
-          <li><a class="greetings" href="#"><p class="greetings-welcome">Welcome, </p><p class="greetings-acc-name"><?php echo $login->getUsername(); ?></p></a></li>
+          <li class="dropdown">
+            <a class="greetings dropdown-toggle" data-toggle="dropdown" href="#">
+              <p class="greetings-welcome">Welcome, </p>
+              <p class="greetings-acc-name">
+                <?php echo $login->getUsername() . " "; ?><span class="caret"></span></p>
+            </a>
+              <ul class="dropdown-menu" role="menu">
+                <li><a href="#">Edit Profile</a></li>
+                <li><a href="#">Change Password</a></li>
+                <li><a href="#">Something else here</a></li>
+              </ul>
+          </li>
           <li><a class="cart" href="#"><span class="badge">3</span><span class="icon-cart"></span><p class="cart-text">My Cart</p></a></li>
         <?php
               }
