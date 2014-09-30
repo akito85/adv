d092659d7c2dae917f6e92e29d3c1dcb86c50171
vitrio wibisono <akito@localhost.localdomain>
2014-10-01 00:10:58 +0700

    revamped about us

--- system/layout/about.php
+++ system/layout/about.php
@@ -1,2 +1,148 @@
-<div class="row about-content">
+<div class="row">
+  <div class="col-xs-12"><img class="img img-responsive" src="../assets/img/about/about-us_01.jpg"/></div>
+  <div class="col-xs-12 about01">
+    <p class="title-head">AUTO</p>
+    <p class="title-head">DE</p>
+    <p class="title-head">VIE</p>
+    <p class="content-tail">Auto De Vie is a place full of passionate and skilled people, who have hearts in this industry, to become the womb where masterpieces born. A playground, a laboratory, a grage, a home, where everyone is invited to fullfil our deepest, wildest dreams to built a state of art!</p>
+  </div>
+  <div class="col-xs-12"><img class="img img-responsive" src="../assets/img/about/about-us_03.jpg"/></div>
+  <div class="col-xs-12 about03">
+    <p class="content-tail">This is the place where wildest ideas born, where challenge ourself into breaking points, dare to dream and make a
+    breakthrough. We reate a playful place, equipped with spirit, passion, love, laughter, where we can develop our talents, expand
+    our thinking and polish our character. Every detail, every aspect of this company aspire the spirit of breakthrough, dignity,
+    brotherhood, to be the pioneer that endure and sustain, so we can express who we really are and inspire the world. Keep
+    evolving to be the best. 
+    </p></div>
+  <div class="col-xs-12">
+    <div id="auto-slider-room" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li class="active" data-target="#auto-slider-room" data-slide-to="1"></li>
+        <li data-target="#auto-slider" data-slide-to="2"></li>
+        <li data-target="#auto-slider" data-slide-to="3"></li>
+        <li data-target="#auto-slider" data-slide-to="4"></li>
+      </ol>
+
+      <!-- Wrapper for slides -->
+      <div class="carousel-inner">
+        <div class="item active">
+          <img src="../assets/img/about/about-us_05.jpg" alt="...">
+          <div class="carousel-caption">
+          </div>
+        </div>
+        <div class="item">
+          <img src="../assets/img/about/about-us_07.jpg" alt="...">
+          <div class="carousel-caption">
+          </div>
+        </div>
+        <div class="item">
+          <img src="../assets/img/about/about-us_08.jpg" alt="...">
+          <div class="carousel-caption">
+          </div>
+        </div>
+        <div class="item">
+          <img src="../assets/img/about/about-us_09.jpg" alt="...">
+          <div class="carousel-caption">
+          </div>
+        </div>
+      </div>
+
+      <!-- Controls -->
+      <a class="left carousel-control" href="#auto-slider-room" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+      </a>
+      <a class="right carousel-control" href="#auto-slider-room" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+      </a>
+    </div>    
+  </div>
+  <!--
+  <div class="col-xs-12"><img class="img img-responsive" src="../assets/img/about/about-us_05.jpg"/></div>
+  <div class="col-xs-12"><img class="img img-responsive" src="../assets/img/about/about-us_07.jpg"/></div>
+  <div class="col-xs-12"><img class="img img-responsive" src="../assets/img/about/about-us_08.jpg"/></div>
+  <div class="col-xs-12"><img class="img img-responsive" src="../assets/img/about/about-us_09.jpg"/></div>
+  -->
+  <div class="col-xs-12 about12"><h2 class="title-head">WHAT WE DO</h2></div>
+  <div class="col-xs-12"><img class="img img-responsive" src="../assets/img/about/about-us_12.jpg"/></div>
+  <div class="col-xs-12 about13">
+    <p class="content-tail">
+      Experience is something that we cant buy. it has to be seen, heard, felt and touched. When you breath the air, and being present
+      to truly feel that moment, that's an experience. Indescribable experience to touch the dreams right into reality, that's something
+      that we concern about.
+    </p>
+    <p class="content-tail">
+      No matter how good a concept is, it will always be a concept. However, Auto De Vie could transfor your concept and dreams into
+      sketches, visualise it, in a way that you had never imagine before, where you can see, fell, even touch and experience your
+      concept right in front of you, even before we built it! Yes, even before we built it! How could that even possible?
+    </p>
+    <p class="content-tail">
+      Inspired by the journey of modern automotive, the spirit to make breakthroughs. We adapt advance technology to create our
+      dreams. We already experience those amazing feelings when our dreams became reality. That's why we want you to experience
+      what we felt. The victorious smile, the winning scent, when the blood rushes into your veins. A jaw dropping experience that
+      never seen before. We brought future to present, to bring present into future.
+    </p>
+    <p class="content-signature">
+      Written by Oscar Widjaja
+    </p>
+    <div class="content-line"></div>
+  </div>
+  <div class="col-xs-12 about14"><p class="title-head">CREWS</p></div>
+  <div class="col-xs-12">
+    <div id="auto-slider-crew" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li class="active" data-target="#auto-slider-crew" data-slide-to="1"></li>
+        <li data-target="#auto-slider" data-slide-to="2"></li>
+        <li data-target="#auto-slider" data-slide-to="3"></li>
+        <li data-target="#auto-slider" data-slide-to="4"></li>
+        <li data-target="#auto-slider" data-slide-to="5"></li>
+      </ol>
+
+      <!-- Wrapper for slides -->
+      <div class="carousel-inner">
+        <div class="item active">
+          <img src="../assets/img/about/about-us_14.jpg" alt="...">
+          <div class="carousel-caption">
+          </div>
+        </div>
+        <div class="item">
+          <img src="../assets/img/about/about-us_15.jpg" alt="...">
+          <div class="carousel-caption">
+          </div>
+        </div>
+        <div class="item">
+          <img src="../assets/img/about/about-us_16.jpg" alt="...">
+          <div class="carousel-caption">
+          </div>
+        </div>
+        <div class="item">
+          <img src="../assets/img/about/about-us_17.jpg" alt="...">
+          <div class="carousel-caption">
+          </div>
+        </div>
+        <div class="item">
+          <img src="../assets/img/about/about-us_18.jpg" alt="...">
+          <div class="carousel-caption">
+          </div>
+        </div>
+      </div>
+
+      <!-- Controls -->
+      <a class="left carousel-control" href="#auto-slider-crew" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+      </a>
+      <a class="right carousel-control" href="#auto-slider-crew" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+      </a>
+    </div>    
+    
+  </div>
+  <!--
+  <div class="col-xs-12"><img class="img img-responsive" src="../assets/img/about/about-us_14.jpg"/></div>
+  <div class="col-xs-12"><img class="img img-responsive" src="../assets/img/about/about-us_15.jpg"/></div>
+  <div class="col-xs-12"><img class="img img-responsive" src="../assets/img/about/about-us_16.jpg"/></div>
+  <div class="col-xs-12"><img class="img img-responsive" src="../assets/img/about/about-us_17.jpg"/></div>
+  <div class="col-xs-12"><img class="img img-responsive" src="../assets/img/about/about-us_18.jpg"/></div>
+  -->
 </div>
