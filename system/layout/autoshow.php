997b08d7a3a9619596e4ea292c371cf93ca5f337
vitrio wibisono <akito@localhost.localdomain>
2014-10-29 11:39:55 +0700

    removing autoshow-content class

--- system/layout/autoshow.php
+++ system/layout/autoshow.php
@@ -1,2 +1,10 @@
-<div class="row autoshow-content">
+<div class="container">
+  <div class="row">
+    <div class="col-xs-12">
+        <div class="blog-date"><h4><strong>August</strong> 05 2014</h4></div>    
+      <a href="../blog/">
+        <img class="img-responsive" src="../assets/img/autoshow/cover/01.jpg">
+      </a>
+    </div>
+  </div>
 </div>
