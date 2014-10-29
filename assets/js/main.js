f8b423819d5c3f8184d24495013e0e78825543dc
vitrio wibisono <akito@localhost.localdomain>
2014-10-29 11:37:02 +0700

    code clean up, added cover for video while ended, and ongoing fix footer click

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,14 +11,14 @@ $(document).ready(function(){
         $('#top-bar, .add-content-margin-top, .content, nav, .adv-footer').removeClass('blur');
     });
 
-    $('#sign-in-btn').on('click', function(){ 	
+    $('#sign-in-btn').on('click', function(){   
         $('.overlay-page').fadeIn();
         $('#sign-up-form').fadeOut();
         $('#sign-in-form').fadeIn();
         $('body').addClass('no-scroll');
         $('#top-bar, .add-content-margin-top, .content, nav, .adv-footer').addClass('blur');
     });
-    $('.sign-up-btn').on('click', function(){	
+    $('.sign-up-btn').on('click', function(){ 
         $('.overlay-page').fadeIn();
         $('#sign-in-form').fadeOut();
         $('#sign-up-form').fadeIn();
@@ -26,7 +26,7 @@ $(document).ready(function(){
         $('#top-bar, .add-content-margin-top, .content, nav, .adv-footer').addClass('blur');
     });
 
-    $('#hq').on('click', function(){  	
+    $('#hq').on('click', function(){    
         $('.overlay-page').fadeIn();
         $('#sign-up-form').fadeOut();
         $('#sign-in-form').fadeOut();
@@ -35,7 +35,7 @@ $(document).ready(function(){
         $('#top-bar, .add-content-margin-top, .content, nav, .adv-footer').addClass('blur');
     });
 
-    $('#edit-clicked').on('click', function(){ 	
+    $('#edit-clicked').on('click', function(){  
         $('.overlay-page').fadeIn();
         $('#sign-up-form').fadeOut();
         $('#sign-in-form').fadeOut();
@@ -45,7 +45,7 @@ $(document).ready(function(){
         $('#top-bar, .add-content-margin-top, .content, nav, .adv-footer').addClass('blur');
     });
 
-    $('#edit-password-clicked').on('click', function(){   	
+    $('#edit-password-clicked').on('click', function(){     
         $('.overlay-page').fadeIn();
         $('#sign-up-form').fadeOut();
         $('#sign-in-form').fadeOut();
@@ -56,38 +56,60 @@ $(document).ready(function(){
         $('#top-bar, .add-content-margin-top, .content, nav, .adv-footer').addClass('blur');
     });
 
-		$('.dropdown').hover(function() {
-		  $('.dropdown-menu').stop(true, true).delay(200).fadeIn();
-		}, function() {
-		  $('.dropdown-menu').stop(true, true).delay(200).fadeOut();
-		});				
+    $('.dropdown').hover(function() {
+      $('.dropdown-menu').stop(true, true).delay(200).fadeIn();
+    }, function() {
+      $('.dropdown-menu').stop(true, true).delay(200).fadeOut();
+    });       
 
     // contact footer hover
-    $('.adv-footer').on('mouseenter', function () {	$('.collapsed-footer').stop(true, true).fadeIn('slow'); });
-    $('.adv-footer').on('mouseleave', function () { $('.collapsed-footer').stop(true, true).fadeOut('slow');});
+    $('.adv-footer').on('mouseenter', function () { $('.collapsed-footer').stop(true, true).fadeIn('slow'); $('.adv-contact-right').show(); });
+    $('.adv-footer').on('mouseleave', function () { $('.collapsed-footer').stop(true, true).fadeOut('slow'); $('.adv-contact-right').hide(); });
 
-    $('.adv-footer').click(function(){$('.collapsed-footer').toggle();});
+    $('.adv-footer').click(function(){
+      $('.collapsed-footer').toggle();
+    });
 
+
+    // video
+    $('#lm').bind('ended', function() {
+      $('#lm')[0].load();
+    });
+
+    $('#wd').bind('ended', function() {
+      $('#wd')[0].load();
+    });
+
+    $('#rc').bind('ended', function() {
+      $('#rc')[0].load();
+    });
+
+    $('#adv').bind('ended', function() {
+      $('#adv')[0].load();
+    });
+        
     // start on pace start
     $('.overlay-loading').fadeIn();
+    $('html, body').addClass('no-scroll');
     Pace.on("done", function() {
         $('.overlay-loading').delay(500).fadeOut("slow");
+        $('html, body').removeClass('no-scroll');
     });
 
     $("#gals").fancybox({
-    	openEffect	: 'elastic',
-    	closeEffect	: 'elastic',
+      openEffect  : 'elastic',
+      closeEffect : 'elastic',
 
-    	helpers : {
-    		title : {
-    			type : 'inside'
-    		}
-    	}
+      helpers : {
+        title : {
+          type : 'inside'
+        }
+      }
     });
 
     $(window).on('scroll', function(){
-    	$("#collapsed").removeClass("in");
-    	$('.collapsed-footer').stop(true, true).fadeOut('slow');
+      $("#collapsed").removeClass("in");
+      $('.collapsed-footer').stop(true, true).fadeOut('slow');
     });
 
 });
